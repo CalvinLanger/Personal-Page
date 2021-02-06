d1af923ea88abad290bbcd62572172148fce7ed1
Bartlomiej Harasim <57857724+CalvinLanger@users.noreply.github.com>
2021-02-06 22:40:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
 
+function blinker() {
+    $('.blinking')
+        .fadeOut(500);
+    $('.blinking')
+        .fadeIn(500);
+}
+setInterval(blinker, 1000);
 
-document.getElementById('intro').innerHTML = "Hello World!"
-
+document.getElementById('intro').innerHTML = "Hello world!"
